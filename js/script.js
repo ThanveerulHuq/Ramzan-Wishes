c83f2ec54e0fac23ff08c8f4cd00777da175e804
Sahil Atahar <sahilatahar@gmail.com>
2023-03-25 10:00:20 +0530

    Update V2

--- js/script.js
+++ js/script.js
@@ -2,18 +2,29 @@ const usernameInput = document.getElementById("username-input");
 const usernameText = document.getElementById('username-text');
 const generateBtn = document.getElementById('generate-btn');
 const shareBtn = document.getElementById('share-btn');
-const enterBtn = document.getElementById('enter-btn');
+const welcomeImage = document.getElementById('welcome-image');
 const audio = document.getElementById("audio");
 const welcomePage = document.getElementById("welcome-page");
+
 
 const getUsername = () => {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     const username = urlParams.get('q');
-    return username;
+    return username != null ? username.replace(/[-]/g, ' ') : username;
+}
+
+const getSubmit = () => {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const submit = urlParams.get('submit');
+    return submit;
 }
 
 const loadPage = (e) => {
+    if (getSubmit() != null) {
+        welcomePage.remove();
+    }
     let username = getUsername();
     if (username != null) {
         usernameText.textContent = username;
@@ -26,11 +37,11 @@ window.onload = loadPage;
 
 shareBtn.addEventListener('click', async () => {
     let link = window.location.href;
-    let shareLink = link.replace(/[+]/g, '%20');
+    let shareLink = link.replace(/[+]/g, '%2D');
     window.open(`whatsapp://send?text=${getUsername() != null ? `*${getUsername()}*` : "Someone"}%0A*Send You a Surprize message*.%0AOpen it once!👇👇%0A${shareLink}`);
 });
 
-enterBtn.addEventListener('click', () => {
+welcomeImage.addEventListener('click', () => {
     welcomePage.classList.add('animate__bounceOutUp');
     audio.play();
 })
